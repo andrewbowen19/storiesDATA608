9776f21564b1bcbb4361150490812b4384d0a28a
andrewbowen19 <atbowen@gmail.com>
2023-10-07 13:16:50 -0400

    Update Story 3 with choropleth

--- Discussion7.R
+++ Discussion7.R
@@ -13,19 +13,21 @@ plot_labels <- labs(x="Car Weight (thousands lbs)",
 p2 <- p1 + plot_labels
 p2
 
-# Flagging japanese branded cars for later plotting
+# Flagging Japanese branded cars for later plotting
 getFirst <- function(l){l[[1]]}
 mtcars$make <- lapply(str_split(rownames(mtcars), " "), getFirst)
 mtcars$is_japanese <- ifelse((mtcars$make == "Mazda") | (mtcars$make == "Honda") | (mtcars$make == "Toyota"),
                              TRUE, FALSE)
 
-# Plot highlighting jkapanese cars
+# Plot highlighting Japanese cars - colored in blue
 p4 <- ggplot(mtcars, aes(x=wt, y=mpg, color=is_japanese)) + geom_point() + 
   labs(x="Car Weight (thousands lbs)",
        y="Miles per Gallon",
        title="MTCars: Fuel Efficiency vs Car Weight",
-       subtitle="Consumers should be aware of the tradeoff between a car's weight and fuel efficiency",
+       subtitle="Consumers should be aware of the tradeoff between a car's weight and fuel efficiency. Japanese manufacturers tend towards the more fuel efficient end of the spectrum",
        color="Japanese Manufacturer?")
 p4
+
+
 
 
